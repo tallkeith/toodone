db1e97b45748d48e4a168b08d9ad8298add1f7ba
tallkeith <tallestkeith@gmail.com>
2016-03-24 16:17:56 -0400

    added todo list migration

---
+++ db/migrate/003_create_todo_lists.rb
@@ -0,0 +1,13 @@
+class CreateTodoLists < ActiveRecord::Migration
+  def up
+    create_table :todo_lists do |t|
+      t.string :list_name, null: false
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :todo_list
+  end
+end
+
